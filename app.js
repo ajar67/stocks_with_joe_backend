dff79051fc534ddabcdd3992293ca9ee9bd0eb08
Anthony Ranieri <anthonyjaranieri@gmail.com>
2024-06-07 14:15:58 -0400

    working python script

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const cors = require("cors");
+const {exec} = require('child_process');
 
 const { PORT = 3003 } = process.env;
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-server.use((req, res, next) => {
+app.post('/run-script', (req, res) => {
+  exec('python test.py', (error, stdout, stderr) => {
+    if(error){
+      return res.status(500).json({error: error.message});
+    }
+    if(stderr){
+      return res.status(500).json({error: stderr})
+    }
+    res.json({output: stdout});
+  })
+});
+
+app.use((req, res, next) => {
   res.status(404).send(`Route ${req.url} not found!`);
 });
 
