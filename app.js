e7485f10e92d3db53eacbc81c1fbf0554ef1a8ee
Anthony Ranieri <anthonyjaranieri@gmail.com>
2024-06-11 11:44:15 -0400

    updates for more routes

--- app.js
+++ app.js
@@ -1,22 +1,46 @@
 const express = require("express");
 const cors = require("cors");
-const {exec} = require('child_process');
+const { exec } = require("child_process");
 
 const { PORT = 3003 } = process.env;
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.post('/run-script', (req, res) => {
-  exec('python test.py', (error, stdout, stderr) => {
-    if(error){
-      return res.status(500).json({error: error.message});
+app.post("/run-script", (req, res) => {
+  exec("python test.py", (error, stdout, stderr) => {
+    if (error) {
+      return res.status(500).json({ error: error.message });
     }
-    if(stderr){
-      return res.status(500).json({error: stderr})
+    if (stderr) {
+      return res.status(500).json({ error: stderr });
     }
-    res.json({output: stdout});
-  })
+    res.status(200).json({ output: stdout });
+  });
+});
+
+app.post("/get-info", (req, res) => {
+  exec("python info.py", (error, stdout, stderr) => {
+    if (error) {
+      return res.status(500).json({ error: error.message });
+    }
+    if (stderr) {
+      return res.status(500).json({ error: stderr });
+    }
+    res.status(200).json({ output: stdout });
+  });
+});
+
+app.post("/kill-script", (req, res) => {
+  exec("python terminate.py", (error, stdout, stderr) => {
+    if (error) {
+      return res.status(500).json({ error: error.message });
+    }
+    if (stderr) {
+      return res.status(500).json({ error: stderr });
+    }
+    res.status(200).json({ output: stdout });
+  });
 });
 
 app.use((req, res, next) => {
